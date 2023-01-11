3a252af4ed0f96d36c3e133349a65a057ee260f1
annalaura008 <emmanuellyalves2@gmail.com>
2023-01-11 18:31:37 -0300

    qulquer coisa 11\01

---
+++ exercicios.js
@@ -0,0 +1,27 @@
+const  gabrielson = -4
+if (gabrielson > 100 || gabrielson < 0) {
+    console.log('numero invalido')
+} else if (gabrielson >=90 ) {
+    console.log('a')
+} else if (gabrielson >=80 ) {
+    console.log('b')
+} else if (gabrielson >=70) {
+    console.log('c')
+} else if (gabrielson >=60) {
+    console.log('d')
+} else if (gabrielson >=50) {
+    console.log('e')
+} else {
+    console.log('f')
+}
+
+
+/* 
+gabrielson >= 90 = A
+gabrielson >= 80 = B
+gabrielson >= 70 = C
+gabrielson >= 60 = D
+gabrielson >= 50 = E
+gabrielson < 50 = F
+gabrielson > 100 ou gabrielson < 0 = ERRO
+*/
